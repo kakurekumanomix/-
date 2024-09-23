652736a28fd7f3ed729586532d5aeb7dd1e132fc
kakurekumanomix <nirday@outlook.jp>
2024-09-23 16:05:17 +0900

    Update socket.js

--- api/socket.js
+++ api/socket.js
@@ -1,20 +1,14 @@
-// api/socket.js
 import { Server } from 'socket.io';
 
 export default function handler(req, res) {
   if (!res.socket.server.io) {
-    // Socket.IOサーバーをセットアップ
     const io = new Server(res.socket.server);
     res.socket.server.io = io;
 
-    // クライアントとの接続
     io.on('connection', (socket) => {
       console.log('新しいクライアントが接続されました');
-
-      // クライアントからのメッセージを受信
       socket.on('message', (msg) => {
-        console.log('クライアントからのメッセージ:', msg);
-        io.emit('message', msg); // 全クライアントにメッセージをブロードキャスト
+        io.emit('message', msg);
       });
     });
   }
